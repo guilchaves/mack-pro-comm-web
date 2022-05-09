d67d49f2b1032c6db5bef36547e05c7109334a5b
guilherme <guilgchaves@protonmail.com>
2022-05-09 18:38:14 -0300

    add try-catch block to async methods

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -2,65 +2,88 @@ const Game = require('../models/Game')
 
 module.exports = {
   async getAll(req, res) {
-    const games = await Game.findAll({
-      include: {
-        association: 'categoria'
-      }
-    })
+    try {
+      const games = await Game.findAll({
+        include: {
+          association: 'categoria',
+        },
+      })
 
-    return res.json(games)
+      return res.json(games)
+    } catch (err) {
+      console.log(err)
+    }
   },
 
   async getGame(req, res) {
-    const { id } = req.params
+    try {
+      const { id } = req.params
 
-    const game = await Game.findByPk(id, {
-      include: {
-        association: 'categoria'
-      }
-    })
+      const game = await Game.findByPk(id, {
+        include: {
+          association: 'categoria',
+        },
+      })
 
-    return res.json(game)
+      return res.json(game)
+    } catch (err) {
+      console.log(err)
+    }
   },
 
   async insert(req, res) {
-    const { nome, descricao, id_categoria, url_jogo, url_imagem, url_demo } = req.body
+    try {
+      const { nome, descricao, id_categoria, url_jogo, url_imagem, url_demo } = req.body
 
-    const game = await Game.create({
-      nome,
-      descricao,
-      id_categoria,
-      url_jogo,
-      url_imagem,
-      url_demo
-    })
+      const game = await Game.create({
+        nome,
+        descricao,
+        id_categoria,
+        url_jogo,
+        url_imagem,
+        url_demo,
+      })
 
-    const newGame = await Game.findByPk(game.id, { include: { association: 'categoria' } })
+      const newGame = await Game.findByPk(game.id, { include: { association: 'categoria' } })
 
-    return res.json(newGame)
+      return res.json(newGame)
+    } catch (err) {
+      console.log(err)
+    }
   },
 
   async update(req, res) {
-    const { nome, descricao, id_categoria, url_jogo, url_imagem, url_demo } = req.body
-    const { id } = req.params
+    try {
+      const { nome, descricao, id_categoria, url_jogo, url_imagem, url_demo } = req.body
+      const { id } = req.params
 
-    await Game.update({
-      nome,
-      descricao,
-      id_categoria,
-      url_jogo,
-      url_imagem,
-      url_demo
-    }, { where: { id } })
+      await Game.update(
+        {
+          nome,
+          descricao,
+          id_categoria,
+          url_jogo,
+          url_imagem,
+          url_demo,
+        },
+        { where: { id } }
+      )
 
-    return res.json({ messege: 'Informações do jogo alteradas' })
+      return res.json({ messege: 'Informações do jogo alteradas' })
+    } catch (err) {
+      console.log(err)
+    }
   },
 
   async delete(req, res) {
-    const { id } = req.params
+    try {
+      const { id } = req.params
 
-    await Game.destroy({ where: { id } })
+      await Game.destroy({ where: { id } })
 
-    return res.json({ messege: 'Jogo deletado' })
-  }
+      return res.json({ messege: 'Jogo deletado' })
+    } catch (err) {
+      console.log(err)
+    }
+  },
 }
